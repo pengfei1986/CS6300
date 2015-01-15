9ac2aef122dcb21efef89f5981af65750dcebfee
Pengfei Sun <psun001@gatech.edu>
2015-01-14 20:34:15 -0500

    remove all

--- Hello.java
+++
@@ -1,6 +0,0 @@
-public class Hello {
-    public static void main (Strings [] args) {
-	System.out.println("Hello java!");
-    }
-
-}
--- hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main()
-{
-  printf("hello C!\n");
-  return 0;
-}
